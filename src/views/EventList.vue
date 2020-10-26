45861d8acf6a2fb8f3980bf83933e0f66284e59c
Gregg Pollack <greggpollack+github@gmail.com>
2020-10-26 15:00:00 -0400

    Made it pretty

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,19 +3,23 @@
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
 
-    <router-link
-      :to="{ name: 'EventList', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-      >Prev Page</router-link
-    >
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Previous</router-link
+      >
 
-    <router-link
-      :to="{ name: 'EventList', query: { page: page + 1 } }"
-      rel="next"
-      v-if="isNextPage"
-      >Next Page</router-link
-    >
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -50,7 +54,7 @@ export default {
     })
   },
   computed: {
-    isNextPage() {
+    hasNextPage() {
       var totalPages = Math.ceil(this.totalEvents / 2)
 
       return this.page < totalPages
@@ -64,5 +68,22 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
