8537d3edaf941e24797900af8ea4a42d978083d4
Gregg Pollack <greggpollack+github@gmail.com>
2020-11-30 12:30:26 -0500

    Removed params id from router-links

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -2,17 +2,11 @@
   <div v-if="event">
     <h1>{{ event.title }}</h1>
     <div id="nav">
-      <router-link :to="{ name: 'EventDetails', params: { id } }"
-        >Details</router-link
-      >
+      <router-link :to="{ name: 'EventDetails' }">Details</router-link>
       |
-      <router-link :to="{ name: 'EventRegister', params: { id } }"
-        >Register</router-link
-      >
+      <router-link :to="{ name: 'EventRegister' }">Register</router-link>
       |
-      <router-link :to="{ name: 'EventEdit', params: { id } }"
-        >Edit</router-link
-      >
+      <router-link :to="{ name: 'EventEdit' }">Edit</router-link>
     </div>
     <router-view :event="event" />
   </div>
