3db2383d06fa79c2e5b9b268b2d03b2d2520a82c
Gregg Pollack <greggpollack+github@gmail.com>
2020-12-14 17:01:29 -0500

    Change /event to /events, with proper redirect for old links

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
-    path: '/event/:id',
+    path: '/events/:id',
     name: 'EventLayout',
     props: true,
     component: EventLayout,
@@ -35,6 +35,12 @@ const routes = [
         component: EventEdit
       }
     ]
+  },
+  {
+    path: '/event/:afterEvent(.*)',
+    redirect: to => {
+      return { path: '/events/' + to.params.afterEvent }
+    }
   },
   {
     path: '/about',
