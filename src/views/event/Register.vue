30537afd85504940857c57dde923a7ff175d9dbc
Gregg Pollack <greggpollack+github@gmail.com>
2021-01-06 16:19:52 -0500

    Added Register Me button and Programmatic Navigation.

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,20 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // Call to API
+      // If registered then redirect to event details
+
+      this.$router.push({
+        name: 'EventDetails'
+      })
+    }
+  }
 }
 </script>
