a16e6e1000a1b0e01963fa4902caa4152e586afb
Gregg Pollack <greggpollack+github@gmail.com>
2021-02-08 15:37:44 -0500

    Added ability to set flash messages

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore)
   .mount('#app')
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,10 +6,18 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       // Call to API
       // If registered then redirect to event details
+
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
 
       this.$router.push({
         name: 'EventDetails'
